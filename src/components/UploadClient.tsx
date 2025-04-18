c0bc32c15ec88465f1a11e6b4d5987a7ba82be51
Jannatul Ferdous <oli1412001@gmail.com>
2025-04-18 21:56:47 +0600

    envwithEslint

--- src/components/UploadClient.tsx
+++ src/components/UploadClient.tsx
@@ -20,7 +20,7 @@ export default function UploadClient() {
       <h3>Upload Image</h3>
       <CldUploadButton
         uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET}
-        onUpload={handleUpload}
+        onSuccess={handleUpload}
       />
     </div>
   );
